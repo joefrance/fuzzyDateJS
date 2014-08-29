6293214d65d5ac0d4365642f324f19708573c5fe
joefrance <joewfrance@yahoo.com>
2014-08-29 08:41:08 -0400

    Added crude stub for framework.

--- fuzzyDate.js
+++ fuzzyDate.js
@@ -2,3 +2,17 @@
  * Created by joefrance on 8/29/14.
  */
 /* Framework for fuzzy dates in JavaScript. */
+
+fuzzyDate = (function () {
+    function FuzzyDate (els) {
+
+    }
+
+    var fuzzyDate = {
+        get: function (selector) {
+
+        }
+    };
+
+    return fuzzyDate;
+}());
